179bcc042448dee21c96e68802a09b9c8eff447b
drymrz <adrymirza@gmail.com>
2022-08-15 12:08:16 +0700

    fixed navbar when user auth

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,61 +1,3 @@
-{{-- <nav class="navbar navbar-expand-lg navbar-dark bg-danger fixed-top">
-    <div class="container">
-        <a class="navbar-brand" href="#">Belajar Laravel</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link {{ $active === 'home' ? 'active' : '' }}" href="/">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ $active === 'about' ? 'active' : '' }}" href="/about">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ $active === 'posts' ? 'active' : '' }}" href="/posts">Blog</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{ $active === 'categories' ? 'active' : '' }}" href="/categories">Categories</a>
-                </li>
-            </ul>
-            <ul class="navbar-nav ms-auto">
-                @auth
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Welcome back , {{ auth()->user()->name }}
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="/dashboard"> <i
-                                        class="bi bi-layout-text-sidebar-reverse"></i> My
-                                    Dashboard</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li>
-                                <form action="/logout" method="post">
-                                    @csrf
-                                    <button type="submit" class="dropdown-item">
-                                        <i class="bi bi-box-arrow-right"></i> Logout
-                                    </button>
-                                </form>
-                            </li>
-                        </ul>
-                    </li>
-                @else
-                    <li class="nav-item">
-                        <a href="/login" class="nav-link {{ $active === 'login' ? 'active' : '' }}">
-                            <i class="bi bi-box-arrow-in-right"></i>
-                            Login</a>
-                    </li>
-                @endauth
-            </ul>
-        </div>
-    </div>
-</nav> --}}
-
 <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center justify-content-between">
         <a href="../" class="logo"><img src="{{ url('img/logo.png') }}" alt="" class="img-fluid" /></a>
@@ -70,11 +12,29 @@
                 <li>
                     <a class="nav-link scrollto  {{ $active === 'posts' ? 'active' : '' }}" href="/posts">Postingan</a>
                 </li>
-                <li>
-                    {{-- <a class="nav-link scrollto" href="#portfolio">Hasil Karya</a> --}}
-                </li>
-                <li><a class="getstarted scrollto" href="/login">Login</a>
-                </li>
+                @auth
+                    <li class="dropdown"><a style="cursor: pointer"><span>Halo , {{ auth()->user()->name }}</span> <i
+                                class="bi bi-chevron-down"></i></a>
+                        <ul>
+                            <li><a href="/dashboard">Dashboard</a>
+                            </li>
+                            <li>
+                                <form action="/logout" method="post">
+                                    @csrf
+                                    <button type="submit" class="dropdown-item" style="padding-left:0">
+                                        <a>
+                                            Logout
+                                        </a>
+                                    </button>
+                                </form>
+                            </li>
+                        </ul>
+                    </li>
+                @else
+                    <li>
+                        <a class="getstarted scrollto" href="/login">Login</a>
+                    </li>
+                @endauth
 
             </ul>
         </nav>
