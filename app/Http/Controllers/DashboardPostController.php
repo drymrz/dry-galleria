99cba75f7e841355a88eeda6cea983e8373c1a70
drymrz <adrymirza@gmail.com>
2022-08-16 12:44:16 +0700

    query change + pagination

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -19,7 +19,7 @@ class DashboardPostController extends Controller
     public function index()
     {
         return view('dashboard.posts.index', [
-            "posts" => Post::where('user_id', auth()->user()->id)->get()
+            "posts" => Post::latest()->where('user_id', auth()->user()->id)->paginate(10)
         ]);
     }
 
